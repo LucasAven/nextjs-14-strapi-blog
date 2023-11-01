1343b596b5dfc63e55458c852542cd24a1778c9f
Lucas Avendano <lavendano@vairix.com>
2023-10-31 21:27:33 -0300

    setup config files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [process.env.NODE_ENV === "development" ? "127.0.0.1" : ""],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
