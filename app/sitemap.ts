c50d526bd53122ac7d2d81dc2b8267f907813cf6
Lucas Avendano <lavendano@vairix.com>
2024-01-26 23:07:23 -0300

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,52 @@
+/* eslint-disable sort-keys */
+import { MetadataRoute } from "next";
+
+import { INTERNAL_ROUTES } from "@/constants/routes";
+import { getCollectionType, StrapiCollectionTypes } from "@/lib/strapi";
+import { getBaseUrl } from "@/lib/utils";
+
+const URL = getBaseUrl();
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { data: authors } = await getCollectionType({
+    contentType: StrapiCollectionTypes.AUTHORS,
+    pagination: { page: 1, pageSize: 99 },
+  });
+
+  const { data: blogs } = await getCollectionType({
+    contentType: StrapiCollectionTypes.BLOGS,
+    pagination: { page: 1, pageSize: 99 },
+  });
+
+  const { data: tags } = await getCollectionType({
+    contentType: StrapiCollectionTypes.TAGS,
+    pagination: { page: 1, pageSize: 99 },
+  });
+
+  return [
+    {
+      url: URL,
+      lastModified: new Date(),
+    },
+    {
+      url: `${URL}${INTERNAL_ROUTES.BLOGS}`,
+      lastModified: new Date(),
+    },
+    ...blogs.map((blog) => ({
+      url: `${URL}${INTERNAL_ROUTES.BLOGS}/${blog.slug}`,
+      lastModified: blog.updatedAt,
+    })),
+    {
+      url: `${URL}${INTERNAL_ROUTES.TAGS}`,
+      lastModified: new Date(),
+    },
+    ...tags.map((tag) => ({
+      url: `${URL}${INTERNAL_ROUTES.TAGS}/${encodeURIComponent(tag.name)}`,
+      lastModified: tag.updatedAt,
+    })),
+    ...authors.map((author) => ({
+      url: `${URL}${INTERNAL_ROUTES.AUTHOR}/${author.slug}`,
+      lastModified: author.updatedAt,
+    })),
+  ];
+}
