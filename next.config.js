10990925d21626bbdb0ee381e13f4c9c5b4736d5
Lucas Avendano <lavendano@vairix.com>
2024-01-26 21:01:10 -0300

    Add experimental web vitals attribution in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    // optimizeCss: true, disabled due to breaking builds
+    webVitalsAttribution: ["CLS", "LCP"],
+  },
   images: {
     remotePatterns: [
       {
